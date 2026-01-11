2da991d64a1a45f8f759f54ab3af8a5efbc464c7
SafakVakti <hesapmarket7@gmail.com>
2026-01-11 22:29:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'safak-vakti-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://www.islamicfinder.org/prayer-times/sounds/adhan/Makkah.mp3' // Ezan sesini telefona indirir
+];
+
+// Dosyaları telefona kaydet (İnternetsiz çalışma için)
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+// Arka planda gelen mesajları ve vaktin geldiğini yönetir
+self.addEventListener('push', e => {
+  const data = e.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'icon.png',
+    ongoing: true // Bildirimi sabit tutar
+  });
+});
 
