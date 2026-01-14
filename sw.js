53518feadd9315352056f484dae39b28f442cc7f
SafakVakti <hesapmarket7@gmail.com>
2026-01-14 18:51:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ const CACHE_NAME = 'safak-vakti-v1';
 const assets = [
   './',
   './index.html',
-  'https://www.islamicfinder.org/prayer-times/sounds/adhan/Makkah.mp3' // Ezan sesini telefona indirir
+  'https://www.islamicfinder.org/prayer-times/sounds/adhan/Makkah.mp3'
 ];
 
-// Dosyaları telefona kaydet (İnternetsiz çalışma için)
+// Dosyaları telefona kaydet
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
@@ -13,10 +13,27 @@ self.addEventListener('install', e => {
 // Arka planda gelen mesajları ve vaktin geldiğini yönetir
 self.addEventListener('push', e => {
   const data = e.data.json();
-  self.registration.showNotification(data.title, {
+  const options = {
     body: data.body,
     icon: 'icon.png',
-    ongoing: true // Bildirimi sabit tutar
-  });
+    badge: 'icon.png', // Android için küçük simge
+    ongoing: true,      // Bildirimi sabit tutar
+    vibrate: [100, 50, 100], // Titreşim desteği
+    requireInteraction: true, // Kullanıcı tıklayana kadar bildirimi genişletmez
+    data: {
+      url: './index.html' // Bildirime tıklayınca uygulamayı açar
+    }
+  };
+
+  e.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
 
+// Bildirime tıklandığında uygulamayı açma özelliği
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(
+    clients.openWindow(e.notification.data.url)
+  );
+});
